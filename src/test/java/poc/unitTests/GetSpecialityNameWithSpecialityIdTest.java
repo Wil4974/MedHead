f32c278ca0ba527e510e66b48a3ae2505962139d
Wil9749 <wilfrid.boudia@outlook.com>
2023-03-17 16:01:45 +0100

    Try to add embedded database https://www.baeldung.com/spring-testing-separate-data-source

--- src/test/java/poc/unitTests/GetSpecialityNameWithSpecialityIdTest.java
+++ src/test/java/poc/unitTests/GetSpecialityNameWithSpecialityIdTest.java
@@ -2,7 +2,12 @@ package poc.unitTests;
 
 import jakarta.annotation.Resource;
 import org.antlr.v4.runtime.misc.LogManager;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import poc.AppMedheadPoc;
+import poc.H2JpaConfig;
 import poc.model.Speciality;
 import poc.repository.SpecialityRepository;
 import poc.util.GetSpecialityIdWithSpecialityName;
@@ -19,62 +24,65 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
+@SpringBootTest(classes = {AppMedheadPoc.class, H2JpaConfig.class})
 public class GetSpecialityNameWithSpecialityIdTest {
 
-        static Instant startedAt;
+    @Autowired
+    private SpecialityRepository specialityRepository;
+    static Instant startedAt;
 
-        static List<Speciality> allSpecialities = new ArrayList<>();
+    static List<Speciality> allSpecialities = new ArrayList<>();
 
-        @BeforeAll
-        static public void initStartingTime() {
-            System.out.println("Appel avant tous les tests");
-            startedAt = Instant.now();
-            allSpecialities.add(new Speciality(1, "Cardiologie"));
-            allSpecialities.add(new Speciality(2,"Immunologie"));
-            allSpecialities.add(new Speciality(3, "Neuropathologie diagnostique"));
-        }
+    @BeforeAll
+    static public void initStartingTime() {
+        System.out.println("Appel avant tous les tests");
+        startedAt = Instant.now();
+        allSpecialities.add(new Speciality(1, "Cardiologie"));
+        allSpecialities.add(new Speciality(2,"Immunologie"));
+        allSpecialities.add(new Speciality(3, "Neuropathologie diagnostique"));
+    }
 
-        @BeforeEach
-        public void getSpecialityNameTest() {
-            System.out.println("Début des tests");
-        }
+    @BeforeEach
+    public void getSpecialityNameTest() {
+        System.out.println("Début des tests");
+    }
 
-        @ParameterizedTest(name = "Id {0} pour la spécialité {1}")
-        @CsvSource({"2, Immunologie", "0, Pas en base", "1, Cardiologie"})
-        public void getSpecialityNameWithSpecialityIdFromFile(int expectResult, String specialityName){
-            //Arrange - See @CsvSource
-            int id;
+    @ParameterizedTest(name = "Id {0} pour la spécialité {1}")
+    @CsvSource({"2, Immunologie", "0, Pas en base", "1, Cardiologie"})
+    public void getSpecialityNameWithSpecialityIdFromFile(int expectResult, String specialityName){
+        //Arrange - See @CsvSource
+        int id;
 
-            //Act
-            id = GetSpecialityIdWithSpecialityName.FromFile(specialityName, allSpecialities);
+        //Act
+        id = GetSpecialityIdWithSpecialityName.FromFile(specialityName, allSpecialities);
 
-            //Asserts
-            assertEquals(expectResult, id);
-        }
-        @ParameterizedTest(name = "Id {0} pour la spécialité {1}")
-        @CsvSource({"2, Immunologie", "0, Pas en base", "1, Cardiologie"})
-        public void getSpecialityNameWithSpecialityIdFromDataBase(int expectResult, String specialityName){
-            //Arrange - See @CsvSource
-            int id;
+        //Asserts
+        assertEquals(expectResult, id);
+    }
+    @ParameterizedTest(name = "Id {0} pour la spécialité {1}")
+    @CsvSource({"2, Immunologie", "0, Pas en base", "1, Cardiologie"})
+    public void getSpecialityNameWithSpecialityIdFromDataBase(int expectResult, String specialityName){
+        //Arrange - See @CsvSource
+        int id;
 
-            //Act
-            id = GetSpecialityIdWithSpecialityName.FromDataBase(specialityName);
+        //Act
+        id = GetSpecialityIdWithSpecialityName.FromDataBase(specialityName);
 
-            //Asserts
-            assertEquals(expectResult, id);
-        }
+        //Asserts
+        assertEquals(expectResult, id);
+    }
 
 
-        @AfterEach
-        public void undefGetSpecialityNameTest() {
-            System.out.println("Fin des tests");
-        }
+    @AfterEach
+    public void undefGetSpecialityNameTest() {
+        System.out.println("Fin des tests");
+    }
 
-        @AfterAll
-        static public void showTestDuration() {
-            System.out.println("Appel après tous les tests");
-            Instant endedAt = Instant.now();
-            long duration = Duration.between(startedAt, endedAt).toMillis();
-            System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
-        }
+    @AfterAll
+    static public void showTestDuration() {
+        System.out.println("Appel après tous les tests");
+        Instant endedAt = Instant.now();
+        long duration = Duration.between(startedAt, endedAt).toMillis();
+        System.out.println(MessageFormat.format("Durée des tests : {0} ms", duration));
+    }
 }
